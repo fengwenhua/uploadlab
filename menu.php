4ce4f0c74faa14e34533526c01a03004f753d218
fengwenhua <13612265462@163.com>
2019-03-16 00:13:36 +0800

    文件上传第三关

--- menu.php
+++ menu.php
@@ -113,7 +113,7 @@
 			<ul class="submenu">
 				<li><a href="<?php echo APP_URL_ROOT;?>/vul/fileupload/Pass-01/index.php">第一关</a></li>
 				<li><a href="<?php echo APP_URL_ROOT;?>/vul/fileupload/Pass-02/index.php">第二关</a></li>
-				<li><a href="#">第三关</a></li>
+				<li><a href="<?php echo APP_URL_ROOT;?>/vul/fileupload/Pass-03/index.php">第三关</a></li>
 			</ul>
 		</li>
 		<li>
---
+++ vul/fileupload/Pass-03/helper.php
@@ -0,0 +1,5 @@
+<?php
+	if($_GET['action'] == 'get_prompt'){
+    	echo '本pass禁止上传.asp|.aspx|.php|.jsp后缀文件！';
+	}
+?>
